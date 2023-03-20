afb78a80abdc9733a89d07b00dc39ac6ea835973
sophiacofone <sophiacofone@gmail.com>
2023-03-20 18:49:55 -0400

    creating basic redux screens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import ProfileScreen from "./screens/profile/profile-screen";
+import store from "./redux/store";
+import HomeScreen from "./screens/home/home-screen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Provider store={store}>
+        <div className="container">
+            <BrowserRouter>
+                <Link to="/">Home</Link> |
+                <Link to="/profile">Profile</Link>
+                <Routes>
+                    <Route path="/" element={<HomeScreen/>} />
+                    <Route path="/profile" element={<ProfileScreen/>} />
+                </Routes>
+              </BrowserRouter>
+        </div>
+      </Provider>
   );
 }
 
