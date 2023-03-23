1070450ff5eb7b5e85b9d94add2055455552643c
sophiacofone <sophiacofone@gmail.com>
2023-03-23 18:29:51 -0400

    f

--- src/soundsynergy/index.js
+++ src/soundsynergy/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import NavigationSidebar from "./navigation-sidebar";
 import {Routes, Route} from "react-router";
 import whoReducer from "./reducers/who-reducer";
 import { configureStore } from '@reduxjs/toolkit';
 import {Provider} from "react-redux";
+import DetailScreen from "./screens/detail/detail-screen";
+import HomeScreen from "./screens/home/home-screen";
+import LoginScreen from "./screens/login/login-screen";
+import SearchScreen from "./screens/search/search-screen";
+import AnalysisScreen from "./screens/analysis/analysis-screen";
 
 const store = configureStore({reducer: {who: whoReducer}});
 
@@ -11,23 +15,16 @@ function SoundSynergy() {
     return (
         <Provider store={store}>
             <div className="row mt-2">
-                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                    <NavigationSidebar active="explore"/>
-                </div>
                 <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                      style={{"position": "relative"}}>
                     <Routes>
                         <Route path="/" element={<HomeScreen />}/>
                         <Route path="home"    element={<HomeScreen/>}/>
                         <Route path="login" element={<LoginScreen/>}/>
-                        <Route path="explore" element={<ExploreComponent/>}/>
-                        <Route path="explore" element={<ExploreComponent/>}/>
-                        <Route path="explore" element={<ExploreComponent/>}/>
-                        <Route path="explore" element={<ExploreComponent/>}/>
+                        <Route path="explore" element={<SearchScreen/>}/>
+                        <Route path="explore" element={<DetailScreen/>}/>
+                        <Route path="explore" element={<AnalysisScreen/>}/>
                     </Routes>
-                </div>
-                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                    <WhoToFollowList/>
                 </div>
             </div>
         </Provider>
---
+++ src/soundsynergy/screens/analysis/analysis-screen.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import {useSelector} from "react-redux";
+
+function AnalysisScreen() {
+    // const {user} = useSelector((state) => state.user);
+    return (
+        <div>
+            <h1>Home</h1>
+            {/*<h2> Welcome {user.firstName} {user.lastName}</h2>*/}
+        </div>
+    );
+}
+
+export default AnalysisScreen;
